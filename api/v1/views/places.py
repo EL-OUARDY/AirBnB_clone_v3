0bdc88d7508c3f07c8e38496a7b9837b701624fb
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-04-02 12:20:49 +0000

    FIX: Api action methods naming issue

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -33,7 +33,11 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route("/places/<place_id>", methods=["DELETE"], strict_slashes=False)
+@app_views.route(
+    "/places/<place_id>",
+    methods=["DELETE"],
+    strict_slashes=False,
+)
 def delete_place(place_id):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -15,7 +15,7 @@ from models.place import Place
 @app_views.route(
     "/places/<place_id>/amenities", methods=["GET"], strict_slashes=False
 )
-def get_amenities(place_id):
+def get_place_amenities(place_id):
     """Retrieves the list of all Amenity objects of a Place"""
     # check if place exists
     place = storage.get(Place, place_id)
@@ -37,7 +37,7 @@ def get_amenities(place_id):
     methods=["DELETE"],
     strict_slashes=False,
 )
-def delete_amenity(place_id, amenity_id):
+def delete_place_amenity(place_id, amenity_id):
     """Retrieves the list of all Amenity objects of a Place"""
 
     # check if place exists
@@ -71,7 +71,7 @@ def delete_amenity(place_id, amenity_id):
     methods=["POST"],
     strict_slashes=False,
 )
-def post_amenity(place_id, amenity_id):
+def post_place_amenity(place_id, amenity_id):
     """Link an Amenity object to a Place"""
     # check if place exists
     place = storage.get(Place, place_id)
